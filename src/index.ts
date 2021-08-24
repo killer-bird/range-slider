0ccf36a41e28e7cdb0b167cef056a0f92e99047a
Rommel <rommel056@gmail.com>
2021-08-25 00:54:08 +0300

    first version of the slider,made a scale, progress bar

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import './scss/index.scss'
 import * as $ from 'jquery'
 import Model from './ts/model'
 
-
 let slider = new Model
 slider.setStep(3)
 console.log(slider.getModel());
@@ -17,24 +16,40 @@ console.log(slider.getModel());
 $('body').greenify()
 
 const mark: HTMLElement = document.getElementsByClassName('range-slider-mark')[0] as HTMLElement
+const rangeSlider: HTMLElement  = document.querySelector('.range-slider')
+const progressBar: HTMLElement = document.querySelector('.progress')
+const scale:HTMLElement = document.querySelector('scale')
+const box = rangeSlider.getBoundingClientRect()
+
+let step :number= 1;
+let min :number= 1;
+let max :number= 1000;
+let values = [];
+let stepCount:number = (max - min)/step
+
+let stepSize = (rangeSlider.offsetWidth - mark.offsetWidth)/stepCount
 
 mark.onmousedown = function (event) {
-
-    moveAt(event.pageX);
-    function moveAt(pageX) {
-        mark.style.left = pageX - mark.offsetWidth / 2 + 'px';
-    }
-    function onMouseMove(event) {
-        moveAt(event.pageX);
-    }
+    event.preventDefault();
+    let shiftX = event.clientX - mark.getBoundingClientRect().left
     document.addEventListener('mousemove', onMouseMove);
-    mark.onmouseup = function() {
-        console.log(124)
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mousedown', onMouseMove)
-        mark.onmouseup = null;
-    };
-}
-mark.ondragstart = function () {
-    return false
+    function onMouseMove(event){
+        let newLeft = event.clientX - shiftX - rangeSlider.getBoundingClientRect().left
+        let left = max - min/ step
+        if (newLeft < 0){
+            newLeft = 0;
+        }
+        let rightEdge = rangeSlider.offsetWidth - mark.offsetWidth
+        if (newLeft > rightEdge) {
+            newLeft = rightEdge;
+        }
+        mark.style.left = newLeft + 'px';
+        mark.innerHTML = Math.round(max*(newLeft / rightEdge * 100/100)) +''
+        progressBar.style.width = Math.round(newLeft/rightEdge*100) + '%'
+        mark.style.color = 'white'
+        mark.style.textAlign = 'center'
+    }
+    document.addEventListener('mouseup', ()=>{
+        document.removeEventListener('mousemove',onMouseMove)
+    })
 }
