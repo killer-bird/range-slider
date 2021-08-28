b38ed981deeed04d84feb0d41959e1f2ec3975d7
Rommel <rommel056@gmail.com>
2021-08-29 02:45:10 +0300

    added step *BETA & scale *BETA

--- src/ts/interface.ts
+++ src/ts/interface.ts
@@ -1,88 +1,88 @@
 export function SliderController(model, view) {
 
     const thumb: HTMLElement = document.querySelector('.range-slider-thumb')
-    const rangeSlider: HTMLElement  = document.querySelector('.range-slider')!;
+    const rangeSlider: HTMLElement = document.querySelector('.range-slider')!;
     const progressBar: HTMLElement = document.querySelector('.range-slider-progress')
-    const scale:HTMLElement = document.querySelector('.range-slider-scale')
+    const scale: HTMLElement = document.querySelector('.range-slider-scale')
     const box = rangeSlider.getBoundingClientRect()
-    let {step,min, max} = model.getModel()
+    let {step, min, max} = model.getModel()
 
 
-    const progressInput:HTMLInputElement = document.querySelector('.colorBar')
-    const progressThumb:HTMLInputElement = document.querySelector('.colorThumb')
+    const progressInput: HTMLInputElement = document.querySelector('.colorBar')
+    const progressThumb: HTMLInputElement = document.querySelector('.colorThumb')
 
-    progressInput.addEventListener('input',()=>{
+    progressInput.addEventListener('input', () => {
         model.setColorBar(progressInput.value)
     })
-    progressThumb.addEventListener('input',()=>{
+    progressThumb.addEventListener('input', () => {
         model.setColorThumb(progressThumb.value)
     })
     rangeSlider.onmousedown = (event) => {
         event.preventDefault()
-        let rightEdge = rangeSlider.offsetWidth - thumb.offsetWidth
-        let clickLeft = event.clientX-rangeSlider.getBoundingClientRect().left
-        if (clickLeft > rightEdge){
-            clickLeft = rightEdge
-        }
-
-        thumb.style.left = clickLeft + 'px'
+        console.log()
         let shiftX = event.clientX - thumb.getBoundingClientRect().left
+        let rightEdge = rangeSlider.offsetWidth - thumb.offsetWidth
+        const rangeKeeper = (count: number, min: number = 0, max: number = rightEdge) => {
+            if (count < min) {
+                return min
+            }
+            if (count > max) {
+                return max
+            }
+            return count
+        }
+        let clickLeft = event.clientX - (thumb.offsetWidth / 2) - rangeSlider.getBoundingClientRect().left
+        console.log(event.clientX)
+        thumb.style.left = rangeKeeper(clickLeft) + 'px'
+
+
         document.addEventListener('mousemove', onMouseMove);
         let newLeft = event.clientX - shiftX - rangeSlider.getBoundingClientRect().left
-        let now = thumb.getBoundingClientRect().left - rangeSlider.getBoundingClientRect().left
+        let current = thumb.getBoundingClientRect().left - rangeSlider.getBoundingClientRect().left
+        progressBar.style.width = Math.round(current / rightEdge * 100) + '%'
+        thumb.innerHTML = rangeKeeper(Math.round(min + (max - min) * (newLeft / rightEdge * 100 / 100)), min, max) + ''
 
-        progressBar.style.width = now / rightEdge *100 + '%'
-        thumb.innerHTML = Math.round(max*(now / rightEdge * 100/100)) +''
 
         function onMouseMove(event) {
-            let now = thumb.getBoundingClientRect().left - rangeSlider.getBoundingClientRect().left
-            let newLeft = event.clientX - shiftX - rangeSlider.getBoundingClientRect().left
-            if (newLeft < 0){
-                newLeft = 0;
-            }
-            console.log(newLeft)
-            let rightEdge = rangeSlider.offsetWidth - thumb.offsetWidth
-            if (newLeft > rightEdge) {
-                newLeft = rightEdge;
-            }
-            console.log(now / rightEdge *100 + ' %')
-            let altshiftX = rightEdge*(step/max * 100/100);
-            thumb.style.left = now  + 'px';
-            progressBar.style.width = now / rightEdge *100 + '%'
-            thumb.innerHTML = Math.round(max*(newLeft / rightEdge * 100/100)) +''
+            let current = thumb.getBoundingClientRect().left - rangeSlider.getBoundingClientRect().left
+            let newLeft = event.clientX - shiftX- rangeSlider.getBoundingClientRect().left
+            let stepLeft = Math.round(newLeft/step)*step
+            console.log(stepLeft)
+            thumb.style.left = rangeKeeper(newLeft + stepLeft) + 'px';
+            progressBar.style.width = Math.round(current / rightEdge * 100) + '%'
+            thumb.innerHTML = rangeKeeper(Math.round(min + (max - min) * (rangeKeeper(newLeft+stepLeft) / rightEdge * 100 / 100)), min, max) + ''
         }
-        document.addEventListener('mouseup', ()=>{
-            document.removeEventListener('mousemove',onMouseMove)
+
+        document.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove', onMouseMove)
         })
 
-
-
     }
-    thumb.onmousedown = function (event) {
-        event.preventDefault()
-        let shiftX = event.clientX - thumb.getBoundingClientRect().left
-        document.addEventListener('mousemove', onMouseMove);
-        function onMouseMove(event){
-            let newLeft = event.clientX - shiftX - rangeSlider.getBoundingClientRect().left
-            console.log(event.clientX)
-            if (newLeft < 0){
-                newLeft = 0;
-            }
-            let rightEdge = rangeSlider.offsetWidth - thumb.offsetWidth
-            if (newLeft > rightEdge) {
-                newLeft = rightEdge;
-            }
-
-            let altshiftX = rightEdge*(step/max * 100/100);
-            console.log(altshiftX)
-            thumb.style.left = newLeft + 'px';
-            thumb.innerHTML = Math.round(max*(newLeft / rightEdge * 100/100)) +''
-            // progressBar.style.width = Math.round(newLeft/rightEdge*100) + '%'
-
-        }
-        document.addEventListener('mouseup', ()=>{
-            document.removeEventListener('mousemove',onMouseMove)
-        })
-    }
+    // thumb.onmousedown = function (event) {
+    //     event.preventDefault()
+    //     let shiftX = event.clientX - thumb.getBoundingClientRect().left
+    //     document.addEventListener('mousemove', onMouseMove);
+    //     function onMouseMove(event){
+    //         let newLeft = event.clientX - shiftX - rangeSlider.getBoundingClientRect().left
+    //         console.log(event.clientX)
+    //         if (newLeft < 0){
+    //             newLeft = 0;
+    //         }
+    //         let rightEdge = rangeSlider.offsetWidth - thumb.offsetWidth
+    //         if (newLeft > rightEdge) {
+    //             newLeft = rightEdge;
+    //         }
+    //
+    //         let altshiftX = rightEdge*(step/max * 100/100);
+    //         console.log(altshiftX)
+    //         thumb.style.left = newLeft + 'px';
+    //         thumb.innerHTML = Math.round(max*(newLeft / rightEdge * 100/100)) +''
+    //         // progressBar.style.width = Math.round(newLeft/rightEdge*100) + '%'
+    //
+    //     }
+    //     document.addEventListener('mouseup', ()=>{
+    //         document.removeEventListener('mousemove',onMouseMove)
+    //     })
+    // }
 
 }
--- src/ts/view.ts
+++ src/ts/view.ts
@@ -1,5 +1,3 @@
-import {MakeObserverSubject} from "./sliderObserver";
-
 export function SliderView(model, rootEl) {
 
     this.slider = $('<div class="range-slider"><div/>').appendTo(rootEl)
@@ -7,16 +5,33 @@ export function SliderView(model, rootEl) {
     this.thumb.css('color', "white")
     this.thumb.css('textAlign', 'center')
     this.progressBar = $('<div class="range-slider-progress"><div/>').appendTo(this.slider)
-    this.scale = $('<div class="range-slider-scale"><div/>').appendTo(this.slider)
+
     model.changed.addObserver(this)
+    this.scale = $('<div class="range-slider-scale"><div/>').appendTo(this.slider)
+    let {min, max, step} = model.getModel()
 
-
-
-   this.subscribe = (func)=>{
-        func(this)
+    for (let i = 0; i <= 20; i++) {
+        if (i === 0 || i % 5 === 0) {
+            this.scaleValue = $('<div class="range-slider-scale-item_text"><div/>').appendTo(this.scale)
+        } else {
+            this.scaleValue = $('<div class="range-slider-scale-item_small"><div/>').appendTo(this.scale)
+        }
+    }
+    let textValue: NodeListOf<HTMLElement> = document.querySelectorAll('.range-slider-scale-item_text')
+    for (let value = min; value <= max; value++) {
+        let counter = 0;
+        console.log(counter)
+        if (value === min || (value / max) * 100 % 25 === 0) {
+            textValue[counter].innerHTML = value
+            counter++
+            console.log(counter)
+        }
     }
 
 
+    this.subscribe = (func) => {
+        func(this)
+    }
 
 
 }
